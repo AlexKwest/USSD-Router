6b8af91e60bfecd5e40acf3577ea528b13f60d0a
AlexKwest <lex.kaname@gmail.com>
2019-04-16 20:47:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#GOIP32 Project
+## USSD command from GOIP32 router
+
+1. Args milisecunde between asks request ussd.
+2. Default args = 30 000 mlsec
+
+- [x] Add USSD command balance
+- [ ] Add UI interface from XAML
+- [ ] Feature SMS request
+
+[https://habr.com/ru/post/180855/](https://habr.com/ru/post/180855/)
+[https://github.com/xoid/goip32](https://github.com/xoid/goip32)
---
+++ UssdLibrary/Helpers/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UssdLibrary.Helpers
+{
+    static class Extensions
+    {
+        public static string UnixTimeNow(this DateTime dateTime)
+        {
+            int unixTime = (int)(dateTime - new DateTime(1970, 1, 1)).TotalSeconds;
+            return Convert.ToString(unixTime, 16);
+        }
+
+        public static DateTime DateTimeFromUnixTime(this double timestamp)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return origin.AddSeconds(timestamp);
+        }
+
+        public static string CreateDirectory(string directoryMain, string directorySub)
+        {
+            string path = directoryMain;
+            string subpath = directorySub;
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            return dirInfo.CreateSubdirectory(subpath).FullName;
+        }
+
+        //TODO: Regex regex = new Regex("([-]?[Минус]?[0-9]+)[,.]?");
+        //public static string RegexResponse(this string usssAnswer)
+        //{
+        //    
+        //    return regex.Match(usssAnswer).Groups[1].ToString();
+        //}
+    }
+}
